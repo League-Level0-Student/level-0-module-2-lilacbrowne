db091571fbb44cb6ef1a10e91e8602cc8b2689cb
lilacbrowne <league@WTS-IM-17.attlocal.net>
2017-12-28 14:29:52 -0800

    nasa

--- src/extra/NasaCountdown.java
+++ src/extra/NasaCountdown.java
@@ -1,9 +1,11 @@
-//    Copyright (c) The League of Amazing Programmers 2013-2017
-//    Level 0
+   //  Copyright (c) The League of Amazing Programmers 2013-2017
+    // Level 0
 
 package extra;
 
- /* NASA wants to shoot a rocket into orbit. 
+import javax.swing.JOptionPane;
+
+/* NASA wants to shoot a rocket into orbit. 
   * Somebody has to do the countdown – or the rocket won’t launch.
  * 
  * The ground control chief will tell you where to start – then count down to 0. 
@@ -14,15 +16,26 @@ package extra;
 
 public class NasaCountdown {
 	public static void main(String[] args) {
-		// 2. Use a pop-up to ask the user where to start counting from
+//2. Use a pop-up to ask the user where to start counting from
+String countdown = JOptionPane.showInputDialog("Where do you want the countdown to start from?");
+int x = Integer.parseInt(countdown);
+for(int i = x; i > 0; i--) {
+	System.out.println(i);
+}
+//3. Change the countdown to use the new starting point
 
-		// 3. Change the countdown to use the new starting point
-		
-		// 1. Print a countdown from 10 to 0 on the console
 
-		// 4. Use the speak method to hear the countdown.
-		
-		// 5. when the counting is done, speak "blastoff!"
+
+// 1. Print a countdown from 10 to 0 on the console
+for(int i = 10; i < 10; i--) {
+	System.out.println(i);
+}
+
+		//4. Use the speak method to hear the countdown.
+	speak(countdown);
+//5. when the counting is done, speak "blastoff!"
+	int i = 0;
+	speak("blastoff!");
 	}
 
 static void speak(String words) {
--- src/extra/VotingBooth.java
+++ src/extra/VotingBooth.java
@@ -1,5 +1,17 @@
 package extra;
 
-public class VotingBooth {
+import javax.swing.JOptionPane;
 
+public class VotingBooth {
+public static void main(String[] args) {
+	String answer = JOptionPane.showInputDialog("How old are you in years?");
+			int x = Integer.parseInt(answer);
+	
+	if (x >= 18) {
+		String president = JOptionPane.showInputDialog("Who do you think the next president should be?");
+		JOptionPane.showMessageDialog(null, "I agree, " + president + " would be a great president!");
+	} else if (x < 18) {
+		JOptionPane.showMessageDialog(null, "Nobody cares what you think!");
+	}
+}
 }
